11fe36328bf1b5c3fdd0243c9204a0f166c38a48
BhanuVard <128610358+BhanuVard@users.noreply.github.com>
2023-10-14 12:21:13 +0530

    pointer to int variable

---
+++ main (7).cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main() {
+ int arr[5] = {1, 2, 3, 4, 5};
+int *ptr = &arr[0];
+cout << *ptr << endl;
+  ptr++;
+  cout << *ptr << endl;
+  return 0;
+}
